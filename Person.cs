7f766fa1c34be3a77cb894b68a75ac77903950de
kietde180701 <155745699+kietde180701@users.noreply.github.com>
2024-09-17 09:56:24 +0700

    123
    123

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿public class Person : IComparable<Person>
+{
+    public int Age { get; set; }
+    public string FName { get; set; }
+    public string LName { get; set; }
+
+    public int CompareTo(Person other)
+    {
+        return this.FName.CompareTo(other.FName);
+    }
+
+    public override string ToString() =>
+        $"Name: {FName} {LName}, Age {Age}";
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>()
+            {
+                new Person { FName = "Vo3", LName = "Kiet", Age = 21 },
+                new Person { FName = "Vo5", LName = "Minh", Age = 1 },
+                new Person { FName = "Vo1", LName = "Kiet", Age = 21 },
+                new Person { FName = "Vo2", LName = "Minh", Age = 1 },
+            };
+
+            Console.WriteLine("Items in list: {0}", list.Count);
+            foreach (Person p in GetEnumerable(list))
+            {
+                Console.WriteLine(p);
+            }
+
+            SortedSet<Person> sorted = new SortedSet<Person>()
+            {
+                new Person { FName = "Vo3", LName = "Kiet", Age = 4 },
+                new Person { FName = "Vo5", LName = "Minh", Age = 6 },
+                new Person { FName = "Vo1", LName = "Kiet", Age = 5 },
+                new Person { FName = "Vo2", LName = "Minh", Age = 1 },
+            };
+
+            Console.WriteLine("Items in sorted set: {0}", sorted.Count);
+            foreach (Person p in GetEnumerable(sorted))
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadLine();
+        }
+
+        // Method returning IEnumerable<T>
+        static IEnumerable<Person> GetEnumerable(IEnumerable<Person> collection)
+        {
+        
+            foreach (var item in collection)
+            {
+                yield return item;
+            }
+        }
+    }
+}
